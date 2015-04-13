c666a0b69419f03d88f22b34516e074e646b8103
Luther Barnum <luther.barnum@gmail.com>
2015-04-12 22:30:12 -0400

    modified defnined create to match spec file

--- manifests/create.pp
+++ manifests/create.pp
@@ -1,30 +1,28 @@
-class mount::create {
-  define mnt_point($lvol,$vg,$pvol,$fstype="ext4",$size,$mnt_opts="defaults",$dump="0",$passno="0") {
-    lvm::volume { $lvol:
-      ensure => present,
-      vg     => "$vg",
-      pv     => "$pvol",
-      fstype => "$fstype",
-      size   => "$size",
-    } ->
-    fstab { $title:
-      source => "/dev/$vg/$lvol",
-      dest   => $title,
-      type   => $fstype,
-      opts   => $mnt_opts,
-      dump   => $dump,
-      passno => $passno,
-    } ->
-    mounts { $title:
-      ensure => present,
-      source => "/dev/$vg/$lvol",
-      dest   => $title,
-      type   => $fstype,
-      opts   => $mnt_opts,
-    } ->
-    exec { "mount_$title":
-      command => "/bin/mount $title",
-      unless  => "/bin/grep -qw $title /proc/mounts",
-    }
+define mount::create($lvol,$vg,$pvol,$size,$fstype='ext4',$mnt_opts='defaults',$dump='0',$passno='0') {
+  lvm::volume { $lvol:
+    ensure => present,
+    vg     => $vg,
+    pv     => $pvol,
+    fstype => $fstype,
+    size   => $size,
+  } ->
+  fstab { $title:
+    source => "/dev/${vg}/${lvol}",
+    dest   => $title,
+    type   => $fstype,
+    opts   => $mnt_opts,
+    dump   => $dump,
+    passno => $passno,
+  } ->
+  mounts { $title:
+    ensure => present,
+    source => "/dev/${vg}/${lvol}",
+    dest   => $title,
+    type   => $fstype,
+    opts   => $mnt_opts,
+  } ->
+  exec { "mount_${title}":
+    command => "/bin/mount ${title}",
+    unless  => "/bin/grep -qw ${title} /proc/mounts",
   }
 }
