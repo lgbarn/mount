b9796fab2ef13c60d515264f6bbdfb9bcde26488
Luther Barnum <luther_barnum@jabil.com>
2015-04-14 15:21:46 -0400

    modified tests and module to pass tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
-describe 'mount' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('mount') }
-  end
+describe 'mount', :type => :class do
+  it { should contain_class('mount') }
 end
