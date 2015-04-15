d02510faef01cae9b96e8c1633a94c21a4bfed0a
Luther Barnum <luther.barnum@gmail.com>
2015-04-14 21:08:34 -0400

    added more spec tests

--- spec/acceptance/mount_check_spec.rb
+++ spec/acceptance/mount_check_spec.rb
@@ -17,5 +17,19 @@ describe 'mount class' do
       apply_manifest(pp, :catch_changes => true)
     end
   end
-  
+
+  describe 'mount::create define type' do
+    describe file('/dev/vg01/lvol1') do
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode 777 }
+    end
+    describe file('/mytest') do
+      it { should be_mounted }
+    end
+    describe command('lvs -a /dev/vg01/lvol1 | grep lvol1 | awk "{print $4}"') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should contain '500.00m' }
+    end
+  end
 end
